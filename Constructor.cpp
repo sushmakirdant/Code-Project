e989a1d2baebb47cca3770e2063858f3ecbf4ef0
sushmakirdant <sushmakirdant70@gmail.com>
2026-04-07 21:30:38 +0530

    Create Constructor.cpp

---
+++ Constructor.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+class Student {
+private:
+    string name;
+    int age;
+
+public:
+    
+    Student(string n, int a) {
+        name = n;
+        age = a;
+    }
+
+    void display() {
+        cout << "Name: " << name << endl;
+        cout << "Age: " << age << endl;
+    }
+};
+
+int main() {
+    Student s1("Alice", 21); 
+    s1.display();
+
+    return 0;
+}
